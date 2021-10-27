d7b9dc4107228488172d412a1e688e94f801dbca
Moshe Katz <moshe.katz@fiverr.com>
2021-10-27 14:56:45 +0300

    desc changes

--- pages/index.js
+++ pages/index.js
@@ -13,30 +13,25 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          מזל טוב לגוגול ❤️
+          גוגול, אהבה - מזל טוב!
         </h1>
 
         <p className={styles.description}>
-          בדיקה בדיקה בדיקה
+          תודה שאת קיימת, כבר לא זוכר מה זה להיות בלעדייך.
         </p>
 
         <p className={styles.description}>
-          עוד קצת בדיקה
+          לחיי עוד הרבה ימי הולדת
+        </p>
+
+        <p className={styles.description}>
+          אני אוהב אותך.
+        </p>
+
+        <p className={styles.description}>
+          גול ❤️
         </p>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
