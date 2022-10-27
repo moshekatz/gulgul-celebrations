66d42d8000e828ec6d9c82b22c8d6586eda58979
Moshe Katz <moshe.katz@fiverr.com>
2022-10-27 19:00:34 +0300

    fixes

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,7 @@ export default function Home() {
       <Head>
         <title>Gulgul celebrations</title>
         <meta name="description" content="Gulgul celebrations" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>❤️</text></svg>">
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>❤️</text></svg>" />
       </Head>
 
       <main className={styles.main}>
